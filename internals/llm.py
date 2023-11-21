dfa294c4e9891965c4f4c508b5724d209ab8c3b5
Philipp Woessner <philipp.woessner@sap.com>
2023-11-21 13:58:37 +0100

    add an agent with tools

--- internals/llm.py
+++ internals/llm.py
@@ -1,7 +1,14 @@
+# langchain
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.llms import Ollama
-from llm_commons.langchain.btp_llm.openai import ChatBTPOpenAI
+
+# sap llm
+from llm_commons.proxy.base import set_proxy_version
+from llm_commons.langchain.proxy import init_llm, init_embedding_model
+
+set_proxy_version("btp")
+# set_proxy_version('aicore') # for an AI Core proxy
 
 
 def ollama(model="llama2"):
@@ -9,5 +16,6 @@ def ollama(model="llama2"):
     return Ollama(model=model, callback_manager=callback_manager)
 
 
-def openAI(model="gpt-4-32k", temperature=0):
-    return ChatBTPOpenAI(deployment_id=model, temperature=temperature)
+# https://github.tools.sap/AI-Playground-Projects/llm-commons
+def sapAI(model="gpt-4", temperature=0):
+    return init_llm(model, temperature=temperature, max_tokens=8000)
